5c25c175da0175c50a8fd8f86e685cb41494d7a3
TheDawgry <ircurlicus123@gmail.com>
2020-11-04 21:14:16 +0100

    Update GeniusModel.cs
    Commented out unused objects

--- APIs/Genius/GeniusModel.cs
+++ APIs/Genius/GeniusModel.cs
@@ -5,55 +5,55 @@ namespace SpotifyLyricsFinder.APIs.Genius
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
     public class Meta
     {
-        public int status { get; set; }
+       //public int status { get; set; }
     }
 
     public class Stats
     {
-        public int unreviewed_annotations { get; set; }
-        public bool hot { get; set; }
-        public int pageviews { get; set; }
+        //public int unreviewed_annotations { get; set; }
+        //public bool hot { get; set; }
+        //public int pageviews { get; set; }
     }
 
     public class PrimaryArtist
     {
-        public string api_path { get; set; }
-        public string header_image_url { get; set; }
-        public int id { get; set; }
-        public string image_url { get; set; }
-        public bool is_meme_verified { get; set; }
-        public bool is_verified { get; set; }
-        public string name { get; set; }
-        public string url { get; set; }
-        public int? iq { get; set; }
+        //public string api_path { get; set; }
+        //public string header_image_url { get; set; }
+        //public int id { get; set; }
+        //public string image_url { get; set; }
+        //public bool is_meme_verified { get; set; }
+        //public bool is_verified { get; set; }
+        //public string name { get; set; }
+        //public string url { get; set; }
+        //public int? iq { get; set; }
     }
 
     public class Result
     {
-        public int annotation_count { get; set; }
-        public string api_path { get; set; }
-        public string full_title { get; set; }
-        public string header_image_thumbnail_url { get; set; }
-        public string header_image_url { get; set; }
-        public int id { get; set; }
-        public int lyrics_owner_id { get; set; }
-        public string lyrics_state { get; set; }
-        public string path { get; set; }
-        public int? pyongs_count { get; set; }
-        public string song_art_image_thumbnail_url { get; set; }
-        public string song_art_image_url { get; set; }
-        public Stats stats { get; set; }
-        public string title { get; set; }
-        public string title_with_featured { get; set; }
+        //public int annotation_count { get; set; }
+        //public string api_path { get; set; }
+        //public string full_title { get; set; }
+        //public string header_image_thumbnail_url { get; set; }
+        //public string header_image_url { get; set; }
+        //public int id { get; set; }
+        //public int lyrics_owner_id { get; set; }
+        //public string lyrics_state { get; set; }
+        //public string path { get; set; }
+        //public int? pyongs_count { get; set; }
+        //public string song_art_image_thumbnail_url { get; set; }
+        //public string song_art_image_url { get; set; }
+        //public Stats stats { get; set; }
+        //public string title { get; set; }
+        //public string title_with_featured { get; set; }
         public string url { get; set; }
-        public PrimaryArtist primary_artist { get; set; }
+        //public PrimaryArtist primary_artist { get; set; }
     }
 
     public class Hit
     {
-        public List<object> highlights { get; set; }
-        public string index { get; set; }
-        public string type { get; set; }
+        //public List<object> highlights { get; set; }
+        //public string index { get; set; }
+        //public string type { get; set; }
         public Result result { get; set; }
     }
 
@@ -64,7 +64,7 @@ namespace SpotifyLyricsFinder.APIs.Genius
 
     public class RootObjectGenius
     {
-        public Meta meta { get; set; }
+        //public Meta meta { get; set; }
         public Response response { get; set; }
     }
 
