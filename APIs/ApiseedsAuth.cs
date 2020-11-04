c19abd97e9ddf59738f1a5e3ea531340cd7b52bb
TheDawgry <ircurlicus123@gmail.com>
2020-11-04 21:09:57 +0100

    Removed bug in commit

--- APIs/ApiseedsAuth.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace SpotifyLyricsFinder.APIs.Apiseeds
-{
-    class ApiseedsAuth
-    {
-        public static string authorizationToken = "KkeVZL0zI9KgcoafSXtgAeKJgBN8F8AwfwKCu9xOWi6qyqdTyaG7qSSNR81FlQmZ";  //deleted from publicly shared code, taken from Apiseeds dashboard
-    }
-}
